9c27df5b8d53362b6527fffe21b6f33e536d76f7
JoseJorgeGC <jjgutierrez910@gmail.com>
2023-06-12 00:39:24 -0400

    Creados los modelos y sus respectivas migraciones.

--- control_une/models.py
+++ control_une/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Circuitos(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 12, null = False)
+    descripcion = models.TextField(max_length = 512)
+    balance_deficit = models.FloatField()
+    numero_de_afectaciones = models.IntegerField()
+    ultima_afectacion = models.DateField(blank = True, null = True)
+
+    def __str__(self):
+        return f"{self.nombre}"
+    
+class Afectaciones(models.Model):
+    id = models.AutoField(primary_key = True)
+    circuito = models.ForeignKey(Circuitos, on_delete = models.CASCADE)
+    fecha = models.DateField()
+    hora_afectacion = models.DateTimeField()
+    hora_restablecido = models.DateTimeField()
+    tiempo_de_afectacion = models.TimeField()
+
+    def __str__(self):
+        return f"{self.circuito} - {self.tiempo_de_afectacion} ({self.fecha})"
+
+class Logs(models.Model):
+    id = models.AutoField(primary_key = True)
+    circuito = models.ForeignKey(Circuitos, on_delete = models.CASCADE)
+    afectado = models.BooleanField()
+    descripcion = models.TextField(max_length = 512)
+    mw_afectados_en_provincia = models.IntegerField()
+    fecha = models.DateField()
+
+    def __str__(self):
+        return f"{self.circuito} {self.afectado} - {self.fecha}"
